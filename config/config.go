3a9adb37657a4d6b0f455e7f5fdf2534f17f36fc
Romeo <romeovs@gmail.com>
2019-10-18 15:59:55 +0200

    Create streams from config

--- config/config.go
+++ config/config.go
@@ -1,28 +1,39 @@
 package config
 
-import (
-	"io"
-)
-
 // Config contains the user configuration of the radio.
 type Config struct {
-	chans []Channel
+	// SayChannelName denotes wether or not the channel name should be
+	// said by the radio when switching channels.
+	SayChannelName bool `json:"say_channel_name"`
+
+	// Channels are all the channels.
+	Channels []*Channel `json:"channels"`
 }
 
-// A Channel is a channel that can be played by the radio player.
-type Channel interface {
-	Stream() io.Reader
+// Channel is the config for a channel.
+type Channel struct {
+	// Type of the channel (webstream, ...)
+	Type string `json:"type"`
+
+	// Name of the channel, will be spoken out loud if SayChannelName is set to true.
+	Name string `json:"name,omitempty"`
+
+	// URL of the webstream or Spotify playlist.
+	URL string `json:"url,omitempty"`
+
+	// The name of the AirPlay device
+	AirplayName string `json:"airplay_name,omitempty"`
 }
 
 // Channel gets the corresponding channel from the config.
-func (cfg *Config) Channel(channel int) Channel {
-	var ch Channel
-	if channel >= 0 && channel < len(cfg.chans) {
-		ch = cfg.chans[channel]
+func (cfg *Config) Channel(channel int) *Channel {
+	var ch *Channel
+	if channel >= 0 && channel < len(cfg.Channels) {
+		ch = cfg.Channels[channel]
 	}
 
 	if ch == nil {
-		return &Silence{}
+		return nil
 	}
 
 	return ch
