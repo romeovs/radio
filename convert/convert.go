c5e3165b5f0ebb24daaaf2953e9cb568fd33c354
Romeo <romeovs@gmail.com>
2019-10-21 18:09:08 +0200

    Set up parameters on conversion

--- convert/convert.go
+++ convert/convert.go
@@ -6,8 +6,17 @@ import (
 	"github.com/romeovs/radio/exec"
 )
 
+var args = []string{
+	"-acodec", "pcm_s16le",
+	"-f", "s16le",
+	"-ac", "2",
+	"-ar", "44000",
+	"pipe:.wav",
+}
+
 // Convert a stream of the specified type in to wav.
 func Convert(typ string, stream io.ReadCloser) (io.ReadCloser, error) {
 	input := "pipe:." + typ
-	return exec.Pipe(stream, "ffmpeg", "-i", input, "pipe:.wav")
+	a := []string{"-i", input}
+	return exec.Pipe(stream, "ffmpeg", append(a, args...)...)
 }
