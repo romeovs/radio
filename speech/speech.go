c9f651a75a5a82651693071d435c3c7c365a2924
Romeo <romeovs@gmail.com>
2019-10-22 10:59:25 +0200

    Convert speech output to correct audio format

--- speech/speech.go
+++ speech/speech.go
@@ -3,10 +3,16 @@ package speech
 import (
 	"io"
 
+	"github.com/romeovs/radio/convert"
 	"github.com/romeovs/radio/exec"
 )
 
 // Say the text.
 func Say(text string) (io.ReadCloser, error) {
-	return exec.Command("espeak", text, "--stdout", "-p", "40", "-s", "160")
+	r, err := exec.Command("espeak", text, "--stdout", "-p", "40", "-s", "160")
+	if err != nil {
+		return nil, err
+	}
+
+	return convert.Convert("wav", r)
 }
