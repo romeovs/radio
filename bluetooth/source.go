dbfb643f86af5ca04663a90d618a932b55e46bda
Romeo <romeovs@gmail.com>
2019-10-29 09:51:12 +0100

    Use list sources from pulseaudio package

--- bluetooth/source.go
+++ bluetooth/source.go
@@ -2,9 +2,9 @@ package bluetooth
 
 import (
 	"errors"
-	"strings"
+	"fmt"
 
-	"github.com/romeovs/radio/exec"
+	"github.com/romeovs/radio/pulseaudio"
 )
 
 var (
@@ -16,21 +16,14 @@ var (
 
 // findSource tries to find the source of the bluetooth command.
 func findSource() (string, error) {
-	out, err := exec.Output("pactl", "list", "sources", "short")
+	sources, err := pulseaudio.ListSources()
 	if err != nil {
 		return "", err
 	}
 
-	return parseSources(string(out))
-}
-
-// parseSources parses the output from pactl and finds the first bluetooth source.
-func parseSources(sources string) (string, error) {
-	lines := strings.Split(sources, "\n")
-	for _, line := range lines {
-		flds := strings.Fields(line)
-		if flds[2] == bluetoothDriver {
-			return flds[0], nil
+	for _, source := range sources {
+		if source.Module == bluetoothDriver {
+			return fmt.Sprintf("%v", source.Index), nil
 		}
 	}
 
