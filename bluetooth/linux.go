95f6eb8c2fbc8cfb836944a8d6d22e3331ca3ab6
Romeo <romeovs@gmail.com>
2019-10-30 11:50:22 +0100

    Poll for bluetooth connection

--- bluetooth/linux.go
+++ bluetooth/linux.go
@@ -4,8 +4,10 @@ package bluetooth
 
 import (
 	"io"
+	"time"
 
 	"github.com/romeovs/radio/exec"
+	"github.com/romeovs/radio/log"
 )
 
 // Bluetooth creates an audio stream that reads from bluetooth devices.
@@ -22,18 +24,7 @@ func Bluetooth(name string) (io.ReadCloser, error) {
 		return nil, err
 	}
 
-	// Find the bluetooth device source name
-	device, err := findSource()
-	if err != nil {
-		return nil, err
-	}
-
-	// Move the source output to bt
-	// This assumes bt is a valid pulseaudio sink
-	err = exec.Run("pactl", "move-source-output", "bt", device)
-	if err != nil {
-		return nil, err
-	}
+	go poll()
 
 	// Listen to bt.monitor from pulseaudio
 	r, err := exec.Command("parec", "-d", "bt.monitor")
@@ -42,6 +33,30 @@ func Bluetooth(name string) (io.ReadCloser, error) {
 	}
 
 	return &bluetoothWrapper{r}, nil
+}
+
+func poll() {
+	for {
+		time.Sleep(500 * time.Millisecond)
+
+		// Find the bluetooth device source name
+		device, err := findSource()
+		if err != nil {
+			// The device is not there, poll again
+			continue
+		}
+
+		log.Info("SWITCHING TO BLUETOOTH SOURCE [%v]", device)
+
+		// Move the source output to bt
+		// This assumes bt is a valid pulseaudio sink
+		// TODO: what is the valid invocation here?
+		err = exec.Run("pactl", "move-sink-input", device, "bt")
+		if err != nil {
+			log.Error("ERROR SWITCHING SINK INPUT: %s", err)
+		}
+		return
+	}
 }
 
 // bluetoothWrapper is a wrapper for io.ReadCloser that powers of the bluetooth module
